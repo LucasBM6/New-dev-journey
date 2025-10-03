c6b854cf2c4823f73ef3a57fe7723c7c8e4377aa
lucas barbosa <lucasbarbosa.medeiros2@gmail.com>
2025-10-03 20:14:32 +0000

    adicionar script `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # New-dev-journey
+
 Meu primeiro repositório para registrar minha jornada como desenvolvedor. Estou iniciando no mundo da tecnologia e usando este espaço para aprender, praticar e evoluir.
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
-function Home(){
-    return <h1>Explorando o código e descobrindo novas formas de criar no mundo digital.</h1>
+function Home() {
+  return (
+    <h1>
+      Explorando o código e descobrindo novas formas de criar no mundo digital.
+    </h1>
+  );
 }
 
 export default Home;
